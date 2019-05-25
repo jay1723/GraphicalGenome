4c3c7aba778b6b1b5030f1ceaa7b47e990e05b59
Jay <jay@jays-mbp.attlocal.net>
2019-05-25 02:45:12 -0400

    added full documentation for all functions in the CCGG.py file

--- CCGG.py
+++ CCGG.py
@@ -5,26 +5,49 @@ import re
 import time
 
 class Nodes(dict):
+
     def __init__(self, inp):
+        """Initialize Nodes object
+        def __init__(self:<Nodes>, inp:<dict>) -> Void
+        """
         self.nodes = inp
     # Override getitem to account for lazy evaluation
     def __getitem__(self,key):
+        """Get node annotations tied to specific node
+        def __getitem__(self:<Nodes>,key:<str>) -> <dict>
+        """
         return self.getNode(key)
     
     def __setitem__(self, key, value):
+        """Creates/Updates the value in the Nodes class dictionary specified by the key
+        def __setitem__(self:<Nodes>, key:<str>, value:<dict>) -> Void
+        """
         self.nodes[key] = value
     
     def __contains__(self, key):
+        """Checks to see whether the given key exists in the Nodes class dictionary
+        def __contains__(self:<Nodes>, key:<str>) -> <bool>
+        """
         return key in self.nodes
             
     def __repr__(self):
+        """Defines console representation of object. Defaults to dictionary representation
+        def __repr__(self:<Nodes>) -> <dict>
+        """
         return self.nodes
     
     def keys(self):
+        """Returns an iterable set of the key values for the Nodes class dictionary
+        def keys(self:<Nodes>) -> <dictview>
+        """
         return self.nodes.keys()
     
     # getNode allows for lazy evaluation
     def getNode(self, nodename):
+        """Returns the dictionary entry for the given nodename. If the header has yet to be parsed for the given key then it 
+        is evaluated and added to the dictionary.
+        def getNode(self:<Nodes>, nodename:<str>) -> <dict>
+        """
         if "hdr" in self.nodes[nodename].keys():
             seq = self.nodes[nodename]["seq"]
             hdr = self.nodes[nodename]["hdr"]
@@ -34,35 +57,54 @@ class Nodes(dict):
             return self.nodes[nodename]
         return self.nodes[nodename]
     
-    def returnDict(self):
-        return self.nodes
-    
     def removeNode(self, nodename):
-        output = self.nodes.pop(nodename, None)
-        if output == None:
-            output = "Key does not exist in dictionary"
-        return output
+        """Remove the given node from the dictionary if it exists. Otherwise return None.
+        def removeNode(self:<Nodes>, nodename:<str>) -> <dict or None>
+        """
+        return self.nodes.pop(nodename, None)
     
 class Edges(dict):
     def __init__(self, inp):
+        """Initialize the Edges object with the provided dictionary
+        def __init__(self:<Edges>, inp:<dict>) -> Void
+        """
         self.edges = inp
         
     def __getitem__(self,key):
+        """Returns the value in the Edges class dictinoary specified by the key.
+        def __getitem__(self:<Edges>, key:<str>) -> <dict>
+        """
         return self.getEdge(key)
     
     def __setitem__(self, key, value):
+        """Updates/Creates the value for the specified key
+        def __setitem__(self:<Edges>, key:<str>, value:<dict>) -> Void
+        """
         self.edges[key] = value
     
     def __contains__(self, key):
+        """Checks to see if the key exists within the Edges class dictionary
+        def __contains__(self:<Edges>, key:<str>) -> <bool>
+        """
         return key in self.edges
     
     def __repr__(self):
+        """Defines the console representation of the object. Defaults to dictionary console representation
+        def __repr__(self:<Edges>) -> <dict>
+        """
         return self.edges
             
     def keys(self):
+        """Returns an iterable set of the key values for the Edges class dictionary
+        def keys(self:<Edges>) -> <dictview>
+        """
         return self.edges.keys()            
     
     def getEdge(self, edgename):
+        """Returns the dictionary entry for the given edgename. If the header has yet to be parsed for the given key then it 
+        is evaluated and added to the dictionary.
+        def getEdge(self:<Edges>, edgename:<str>) -> <dict>
+        """
         if "hdr" in self.edges[edgename].keys():
             seq = self.edges[edgename]["seq"]
             hdr = self.edges[edgename]["hdr"]
@@ -71,31 +113,52 @@ class Edges(dict):
             self.edges[edgename]["seq"] = seq
             return self.edges[edgename]
         return self.edges[edgename]    
-       
-    def returnDict(self):
-        return self.edges
     
     def removeEdge(self, edgename):
-        output = self.edges.pop(edgename, None)
-        if output == None:
-            output = "Key does not exist in dictionary"
-        return output
+        """Remove the given edge from the dictionary if it exists. Otherwise return None.
+        def removeEdge(self:<Edges>, edgename:<str>) -> <dict or None>
+        """
+        return self.edges.pop(edgename, None)
 
 class GraphicalGenome:
     
-    def __init__(self, nodefile, edgefile, verbose=False):
+    def __init__(self, nodefile, edgefile, verbose=False, enamelen=12, nnamelen=12):
+        """Initializes the Graphical Genome data structure. Requires only 2 positional parameters: nodefile and edgefile
+        def __init__(self:<GraphicalGenome>, nodefile:<str>, edgefile:<str>, 
+                        verbose=False:<bool>, enamelen=12:<int>, nnamelen:<int>) -> Void
+        Parameters:
+            Nodefile: <str> - Path to the node fasta file
+            Edgefile: <str> - Path to the edge fasta file
+            verbose: <bool> - True for verbose print statements, False for silent
+            enamelen: <int> - Length of the edge key names contained in the edges dictionary
+            nnamelen: <int> - Length of the node key names contained in the nodes dictionary
+        """
+         # Global variables
+        self.edgenamelength = enamelen
+        self.nodenamelength = nnamelen
+        
         self.verbose = verbose
         self.nodes, self.edges, self.outgoing, self.incoming, self.max_node_name, self.max_edge_name, self.genes = self.loadChromosomeGraph(nodefile, edgefile)
         self.nodes = Nodes(self.nodes)
         self.edges = Edges(self.edges)
-        
+    
+           
     def __repr__(self):
+        """Returns the console representation of the Graphical Genome Class. Returns the number of nodes and edges.
+        def __repr__(self:<GraphicalGenome>) -> <str>
+        """
         return "Number of Nodes %d - Number of Edges %d" % (len(self.nodes), len(self.edges))
         
     def __str__(self):
+        """Defines representation of Graphical Genome when using the print statement. Returns the nodefile/edgefile paths
+        def __str__(self:<GraphicalGenome>) -> <str>
+        """
         return "Origin - Nodefile %s    Edgefile %s" % (nodefile, edgefile)
     
     def encodeASCII(self, data):
+        """Encodes the data as a dictionary in ascii format. 
+        def encodeASCII(self:<GraphicalGenome>, data:<dict>) -> <dict>
+        """
         return dict((k.encode('ascii'), [i.encode('ascii') for i in v] if isinstance(v, list) else v.encode('ascii')) for k, v in data.items())
 
     
@@ -105,6 +168,24 @@ class GraphicalGenome:
     # edgefile - Absolute path to the edgefile you wish to open
 # Outputs the nodes and edges as dictionaries as well as auxilary dictionaries containing node -> list(edge) pairs 
     def loadChromosomeGraph(self, nodefile, edgefile):
+        """Reads in the nodefile/edgefile fasta files and parses them into the GraphicalGenome class objects
+        def loadChromosomeGraph(self:<GraphicalGenome>, nodefile:<str>, edgefile:<str>) -> <dict,dict,dict,dict,int,int,dict>
+        
+        Parameters:
+            nodefile: <str> - Path to the node FASTA file
+            edgefile: <str> - Path to the edge FASTA file
+           
+        Output:
+            nodes: <dict> - Nodes dictionary
+            edges: <dict> - Edges dictionary
+            sources: <dict> - Dictionary of the lists of source nodes with the edgenames as keys
+            destinations: <dict> - Dictionary of the lists of destination nodes with edgenames as keys
+            max_node: <int> - The maximum node number found during parsing
+            max_edge: <int> - The maximum edge number found during parsing
+            
+            Not in use:
+            genes: <dict> - Dictionary containing list of nodes on which a given node is found. Gene names are the keys
+        """
         # Instantiate returned datastructures
         max_node = 0
         max_edge = 0
@@ -120,8 +201,8 @@ class GraphicalGenome:
             hdr = fp.readline()[1:]
             seq = fp.readline()
             while hdr and seq:
-                node =  hdr[0:12]
-                nodehdr = hdr[13:]
+                node =  hdr[0:self.nodenamelength]
+                nodehdr = hdr[self.nodenamelength+1:]
                 nodes[node] = {}
                 # Strip newlines if they exist at the end
                 if nodehdr[-1] == "\n":
@@ -148,17 +229,28 @@ class GraphicalGenome:
         seh = time.time()        
         # Edge iteration
         with open(edgefile, "r") as fp:
+            # Calculating the positions of the source and dst nodes
+            sstart = self.edgenamelength + 9
+            send = sstart + self.nodenamelength
+            dstart = send + 9
+            dend = dstart + self.nodenamelength
+            
+            # Read in the data and parse 
             hdr = fp.readline()[1:]
             seq = fp.readline()
             while hdr and seq:
                 # Extract the src, dst, key from the header
-                src = hdr[21:33]
-                dst = hdr[42:54]
-                key = hdr[0:12]
+                src = hdr[sstart:send]
+                dst = hdr[dstart:dend]
+                key = hdr[0:self.edgenamelength]
                 
                 # Source and sink nodes are not the same size as other nodes so special case needed
                 if "SOURCE" in src:
-                    dst = hdr[36:48]
+                    # "SOURCE" is 6 letters long so change the values of the indices to reflect this
+                    send = sstart + 6
+                    dstart = send + 9
+                    dend = dstart + self.nodenamelength
+                    dst = hdr[dstart:dend]
                     src = "SOURCE"
                 if "SINK" in dst:
                     dst = "SINK"
@@ -170,7 +262,7 @@ class GraphicalGenome:
                 if seq[-1] == "\n":
                     seq = seq[:-1]
                 edges[key] = {}
-                edges[key]["hdr"] = hdr[13:]
+                edges[key]["hdr"] = hdr[self.edgenamelength+1:]
                 edges[key]["seq"] = seq
                 
                 # Lazy eval the sources and destinations dictionary
@@ -196,6 +288,15 @@ class GraphicalGenome:
         return nodes, edges, sources, destinations, max_node, max_edge, genes
     
     def writeFasta(self, filename, input_dict, keylist=["src", "dst"]):
+        """Write the given node or edge file as a FASTA file. Overwrites existing file. Will create file if it doesn't exist. 
+        def writeFasta(self:<GraphicalGenome>, filename:<str>, input_dict:<dict>, keylist=["src","dst"]:<list[str]>) -> Void
+        
+        Parameters:
+            filename: <str> - absolute path for the file you wish to write. 
+            input_dict: <dict> - Node or Edge dictionary you wish to write. 
+            keylist: <list[str]> - list of strings of dictionary keys that you want to ignore during write. 
+                    This will require you to write these values on your own or just discard them. 
+        """
         sorted_keys = sorted(input_dict.keys()) 
         with open(filename, "w+") as fastafile:
             # If iterating through the edges, write the edges in the correctly ordered format
@@ -235,6 +336,13 @@ class GraphicalGenome:
         
 
     def returnNodeEdgePath(self, strain, chromo):
+        """Returns a list of [node,edge,node,edge...] that traces out a path for a strain on a given chromo
+        def returnNodeEdgePath(self:<GraphicalGenome>, strain:<str>, chromo:<str>) -> list[str]
+        
+        Parameters:
+            strain: <str> - founder or CC strain you wish to trace. 
+            chromo: <str> - chromosome you are iterating through
+        """
         edges = self.edges
         nodes = self.nodes
         outgoing = self.outgoing
@@ -260,14 +368,19 @@ class GraphicalGenome:
                         currentEdge = edge 
         return path 
     
-# Given a CC path, look for all node pairs that do not contain an edge between them that has the specified CC strain on them. 
-# * Parameters: 
-#     * (str) strain - Strain you wish to trace 
-# * (str) chromo - Chromosome you are tracing the strain through 
-# * Output: 
-#     * list(tuple(node, node)) - List of node tuples that do not have any edges between them with the specified CC strain on them.
-# THIS CURRENTLY WORKS FOR EDGES WITHOUT FLOATING NODES, WHEN WE REINTRODUCE FLOATING NODES THIS NEEDS TO BE FIXED
+
+# 
     def findMissingPaths(self, strain, chromo):
+        """Given a CC path, look for all node pairs that do not contain an edge between them that has the specified CC strain on them.
+        def findMissingPaths(self:<GraphicalGenome>, strain:<str>, chromo:<str>) -> <list[tuple(node,node)]>
+        
+        Parameters:
+            strain: <str> - Strain you wish to trace 
+            chromo: <str> - Chromosome you are tracing the strain through 
+        
+        Notes:
+            THIS CURRENTLY WORKS FOR EDGES WITHOUT FLOATING NODES, WHEN FLOATING NODES ARE ADDED THIS NEEDS TO BE UPDATED
+        """
         nodePairs = set()
         visited = set()
         for i in self.nodes:
@@ -289,6 +402,13 @@ class GraphicalGenome:
 
     # Reconstruct an entire sequence for a given strain across the entire genome
     def reconstructSequence(self, strain, path=0):
+        """Reconstruct the sequence given a strain (ABCDEFGH or CC) and can return more than one sequence if heterozygosity is known
+        def reconstructSequence(self:<GraphicalGenome>, strain:<str>, path:<int>) -> <str>
+                
+        Parameters:
+            strain: <str> - ABCDEFGH or CC strain you wish to trace
+            path: <int> - 0 for base path, 1 for first heterozygous path identified, 2 for second heterozygous path identified. 
+        """
         nodes = self.nodes
         edges = self.edges
         outgoing = self.outgoing
@@ -339,12 +459,22 @@ class GraphicalGenome:
 
     # Used in the CLI, writes a sequence to a specified file
     def writeSequence(self, strain, filedest, path=0):
+        """CLI function that writes a specified sequence to a file. 
+        def writeSequence(self:<GraphicalGenome>, strain:<str>, path:<int>):
+        
+        Parameters:
+            strain: <str> - ABCDEFGH or CC strain you wish to trace
+            path: <int> - 0 for base path, 1 for first heterozygous path identified, 2 for second heterozygous path identified. 
+        """
         reconstructed = self.reconstructSequence(strain, path)
         with open(filedest, "w+") as seqfile:
             seqfile.write(reconstructed)
             
     # Given an anchor, provide the next anchor
     def nextAnchor(self, node):
+        """Return the next monotonically incrementing anchor given a nodename in the graph
+        def nextAnchor(self:<GraphicalGenome>, node:<str>) -> <str>
+        """
         while True:
             edge = self.outgoing[node][0]
             if self.edges[edge]["dst"][0] == "A" or self.edges[edge]["dst"] == "SINK":
@@ -352,8 +482,16 @@ class GraphicalGenome:
             else:
                 node = self.edges[edge]["dst"]
                 
-    # Return a [node, edge, node...] path tracing a strain between two anchors
+    # 
     def tracePath(self, strain, start, end):
+        """Return a [node, edge, node...] path tracing a strain between two anchors
+        def tracePath(self:<GraphicalGenome>, strain:<str>, start:<str>, end:<str>):
+        
+        Parameters:
+            strain: <str> - Founder or CC path that you want to follow. 
+            start: <str> - Start anchor name
+            end: <str> - End anchor name. Must occur later in the genome than the start anchor.
+        """
         startAnchor = start
         endAnchor = end
         path = []
@@ -367,6 +505,3 @@ class GraphicalGenome:
         path.append(endAnchor)
         return path
     
-    
-    # Bounding Anchors
-        # Given 
